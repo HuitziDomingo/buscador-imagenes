c974bffc2c9e7d64b308ec75f0697d65ebe9113c
HuitziDomingo <huitzitzilidomingo@gmail.com>
2021-11-07 23:45:27 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
+import { useState } from 'react'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
+const App = () => {
+
+  const [photos, setphotos] = useState([])
+  console.log({ photos })
+
+  const open = url => window.open(url)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            let response = await fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID 0vaAfVDuv3BGYsi6htorOHo9bHSaKbfCdIG7u02-Cmo',
+              }
+            })
+            try {
+              let data = await response.json()
+              setphotos(data.results)
+              console.log(data)
+            } catch (error) {
+              console.log(error)
+            }
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
+      {/* Cuerpo */}
+      <div className="container">
+        <div className="center">
+          {
+            photos.map(x => (
+              <article key={x.id} onClick={() => open(x.links.html)} >
+                <img src={x.urls.regular} alt={x.urls.regular} />
+                <p>{[x.description, x.alt_description].join(' - ')}</p>
+              </article>
+            ))
+          }
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
